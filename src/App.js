4e883d7e85bbd39b18824b90e38697d1b03b2008
[suzan naeem] <[suzannaim7@gmail.com]>
2021-08-02 22:51:39 +0200

    chat_application

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '055e340d-2b39-4e21-8893-be908194eaf1';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      // userName="suzan naeem"
+      // userSecret="suzan_secret"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// logout , remove msg , inf scroll
+
+export default App;
